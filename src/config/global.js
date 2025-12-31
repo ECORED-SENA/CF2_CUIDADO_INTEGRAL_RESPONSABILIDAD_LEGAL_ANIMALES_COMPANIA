6c2ed1c076ea9de85a4557b5d3c3a1372c0a7e48
Cielo Angulo <cielodamarisangulo@gmail.com>
2025-12-30 23:23:36 -0500

    update

--- src/config/global.js
+++ src/config/global.js
@@ -169,7 +169,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/XX_CF02_DU.pdf',
       },
       {
         icono: 'fas fa-download',
